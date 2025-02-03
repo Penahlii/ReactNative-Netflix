0cd48cccf1122e9574adf28963cbb9578cfc722d
Penahli Ibrahim <penahliibrahim58@gmail.com>
2025-02-03 21:22:47 +0400

    Done

---
+++ src/stacks/TabStack.jsx
@@ -0,0 +1,9 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+const TabStack = () => {
+  return <Tab.Navigator>{/* Add your screens */}</Tab.Navigator>;
+};
+
+export default TabStack;
